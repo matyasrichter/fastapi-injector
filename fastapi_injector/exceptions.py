05ab2612d3e005c250fc232b0e7be220ab245fea
Matyas Richter <matyas@mrichter.cz>
2022-06-16 17:20:52 +0200

    Split the library into multiple files

---
+++ fastapi_injector/exceptions.py
@@ -0,0 +1,2 @@
+class InjectorNotAttached(ValueError):
+    """Raised when Injected is used without an injector instance attached to the app."""
---
+++ fastapi_injector/injected.py
@@ -0,0 +1,19 @@
+from typing import Any, TypeVar
+
+from fastapi import Depends, Request
+
+from fastapi_injector.attach import get_injector_instance
+
+BoundInterface = TypeVar("BoundInterface", bound=type)
+
+
+def Injected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+    """
+    Asks your injector instance for the specified type,
+    allowing you to use it in the route.
+    """
+
+    def inject_into_route(request: Request) -> BoundInterface:
+        return get_injector_instance(request.app).get(interface)
+
+    return Depends(inject_into_route)
