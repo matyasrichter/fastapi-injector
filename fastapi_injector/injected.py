3ccdd901f818aabb22374f2e75378d8df3946312
Ghennadi Colievschii <ghennadi@gr4vy.com>
2022-12-19 15:50:42 +0700

    make inject_into_route async

--- fastapi_injector/injected.py
+++ fastapi_injector/injected.py
@@ -13,6 +13,19 @@ def Injected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
     allowing you to use it in the route.
     """
 
+    async def inject_into_route(request: Request) -> BoundInterface:
+        return get_injector_instance(request.app).get(interface)
+
+    return Depends(inject_into_route)
+
+
+def SyncInjected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+    """
+    Asks your injector instance for the specified type,
+    allowing you to use it in the route. Intended for use
+    with synchronous interfaces.
+    """
+
     def inject_into_route(request: Request) -> BoundInterface:
         return get_injector_instance(request.app).get(interface)
 
