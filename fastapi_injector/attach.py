3ba2a54ccdb2b3ab5771bec5efbfb08e7531b18d
Rich Tebb <rich@tebb.io>
2023-11-16 09:14:06 +0000

    Default RequestScopeOptions on attach()

--- fastapi_injector/attach.py
+++ fastapi_injector/attach.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import FastAPI
 from injector import Injector, InstanceProvider, singleton
 
@@ -8,13 +6,14 @@ from fastapi_injector.request_scope import RequestScopeOptions
 
 
 def attach_injector(
-    app: FastAPI, injector: Injector, options: Optional[RequestScopeOptions] = None
+    app: FastAPI,
+    injector: Injector,
+    options: RequestScopeOptions = RequestScopeOptions(),
 ) -> None:
     """
     Call this function on app startup to attach an injector to the app.
     """
     app.state.injector = injector
-    options = options or RequestScopeOptions(enable_cleanup=False)
     injector.binder.bind(
         RequestScopeOptions, InstanceProvider(options), scope=singleton
     )
