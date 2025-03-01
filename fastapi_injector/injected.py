13092524c3834b25a38c7d49c09c778505bd0076
Eiríkur Torfason <eirikur@neckcare.com>
2025-03-01 10:18:05 +0000

    Provide an accurate return type for Injected and SyncInjected
    As a result, it's no longer necessary to specify the type of parameters for type checkers that infer parameter types by default arguments (e.g. PyRight). Hence,
    def foo(bar: Bar = Injected(Bar)):
    becomes
    def foo(bar=Injected(Bar)):
    without loss of type information.

--- fastapi_injector/injected.py
+++ fastapi_injector/injected.py
@@ -1,33 +1,33 @@
-from typing import Any, TypeVar
+from typing import Any, Type, TypeVar
 
 from fastapi import Depends
 from starlette.requests import HTTPConnection
 
 from fastapi_injector.attach import get_injector_instance
 
-BoundInterface = TypeVar("BoundInterface", bound=type)
+T = TypeVar("T")
 
 
-def Injected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+def Injected(interface: Type[T]) -> T:
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route.
     """
 
-    async def inject_into_route(conn: HTTPConnection) -> BoundInterface:
+    async def inject_into_route(conn: HTTPConnection) -> T:
         return get_injector_instance(conn.app).get(interface)
 
     return Depends(inject_into_route)
 
 
-def SyncInjected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+def SyncInjected(interface: Type[T]) -> T:
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route. Intended for use
     with synchronous interfaces.
     """
 
-    def inject_into_route(conn: HTTPConnection) -> BoundInterface:
+    def inject_into_route(conn: HTTPConnection) -> T:
         return get_injector_instance(conn.app).get(interface)
 
     return Depends(inject_into_route)
