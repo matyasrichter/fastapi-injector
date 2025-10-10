27bc0431f47b0986c0d827ff40c910acc8331ce3
Matyas Richter <matyas@mrichter.cz>
2025-10-10 17:06:11 +0200

    Exclude sync-async trycatch from coverage

--- fastapi_injector/request_scope.py
+++ fastapi_injector/request_scope.py
@@ -119,7 +119,8 @@ class RequestScope(InjectorScope):
         # thread blocks
         try:
             asyncio.get_running_loop()
-        except RuntimeError:  # 'RuntimeError: There is no current event loop...'
+        except RuntimeError:  # pragma: no cover
+            # 'RuntimeError: There is no current event loop...'
             # Starting new event loop
             asyncio.run(stack.enter_async_context(obj))
         else:
